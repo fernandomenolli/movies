863b6128507ebf9fe123c280dd87c738992b37dd
teacherroger <rogerioferreira.jr@gmail.com>
2024-05-20 09:29:56 -0300

    slc2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
-  enum :genre, %i[action news adventure drama comedy horror]
+  enum :genre, %i[action adventure drama comedy horror news]
+
+  def self.rogers_order
+    %i[action news adventure drama comedy horror]
+  end
 end
