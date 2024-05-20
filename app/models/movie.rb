1f418b8640c3117fa2b06c52eb83d69b3505ba26
teacherroger <rogerioferreira.jr@gmail.com>
2024-05-20 19:26:42 -0300

    made it more elgant under Fer supervision

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,10 +4,6 @@ class Movie < ApplicationRecord
   enum :genre, %i[action adventure drama comedy horror news]
 
   def self.rogers_order
-    %i[action news adventure drama comedy horror]
-  end
-
-  def self.translated_genres_in_order
-    rogers_order.map { |genre| [I18n.t("activerecord.attributes.movie.genres.#{genre}"), genre] }
+    %i[action news adventure drama comedy horror].map { |genre| [I18n.t("activerecord.attributes.movie.genres.#{genre}"), genre] } 
   end
 end
